1065c0b9f6b0dfc24b2376dfdca627dc90d5b6dc
Sam Achola <sam.achola@live.com>
2018-03-09 16:28:40 +0300

    Set up routes and Landing route

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
+import Routes from './components/routes/routes';
 
-ReactDOM.render(<Router />, document.getElementById('root'));
+
+ReactDOM.render(<Routes />, document.getElementById('root'));
 registerServiceWorker();
