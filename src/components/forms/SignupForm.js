e6b5e76bda99389e1486681bdb32d0741aabac7f
Sam Achola <sam.achola@live.com>
2018-03-13 20:15:08 +0300

    Signup request with validation.

--- src/components/forms/SignupForm.js
+++ src/components/forms/SignupForm.js
@@ -1,59 +1,72 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 
-import API_URL from '../config/config';
-
-class SignupForm extends Component{
-
+class SignupForm extends Component {
     constructor(props){
         super(props);
         this.state = {
             'email': '',
             'username': '',
-            'password': ''
+			'password': '',
+			'errors': {}, 
+			'isLoading': false
         }
 
         this.onChange = this.onChange.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
     }
 
-    onChange(e){
-        this.setState({ [e.target.name ]: e.target.value });
-    }
+	onChange = (e) => {
+			this.setState({ [e.target.name ]: e.target.value });
+	}
 
-    onSubmit(e){
-        e.preventDefault();
-        //axios.post(API_URL+'/auth/register', this.state)
-        this.props.userSignupRequest(this.state)
-        
-    }
-    render(){
+	onSubmit = (e) => {
+			e.preventDefault();
+			this.setState({ errors: {}, isLoading: true });
+			this.props.userSignupRequest(this.state).then(
+				(response) => {
+					console.log(response.data);
+				}
+			).catch((error) => {
+				console.log(error.response.data.errors);
+				this.setState({ errors: error.response.data.errors, isLoading: false });
+			});	
+	}
 
-        return (
-            <form onSubmit={ this.onSubmit }>
-                <div className="inputwrapper">
-                    <label>Email</label>
-                    <input type="email" value={this.state.email} name="email" id="email" onChange={this.onChange} />
-                </div>
+	render(){
+		const { errors } = this.state;
+		return (
+				<form onSubmit={ this.onSubmit }>
+						<div className="inputwrapper">
+								<label>
+									Email 
+									{ errors.email && <span className="errs">{ errors.email[0] }</span> }
+								</label>
+								<input type="email" value={this.state.email} name="email" id="email" onChange={this.onChange} />
+						</div>
 
-                <div className="inputwrapper">
-                    <label>Username</label>
-                    <input type="text" value={this.state.username} name="username"  id="username" onChange={this.onChange} />
-                </div>
+						<div className="inputwrapper">
+								<label>
+									Username 
+									{ errors.username && <span className="errs">{ errors.username[0] }</span> }
+								</label>
+								<input type="text" value={this.state.username} name="username"  id="username" onChange={this.onChange} />
+						</div>
 
-                <div className="inputwrapper">
-                    <label>Password</label>
-                    <input type="password" value={this.state.password} name="password"  id="password" onChange={this.onChange} />
-                </div>
+						<div className="inputwrapper">
+								<label>
+									Password 
+									{ errors.password && <span className="errs">{ errors.password[0] }</span> }
+								</label>
+								<input type="password" value={this.state.password} name="password"  id="password" onChange={this.onChange} />
+						</div>
 
-                <div className="login-action">
-                    <input type="submit" value="signup" />
-                    <a href="#">Forgot Password..?</a>
-                </div>
-            </form>
-        );
-
-    }
+						<div className="login-action">
+								<input disabled={ this.state.isLoading } type="submit" value="signup" />
+								<a href="#">Forgot Password..?</a>
+						</div>
+				</form>
+		);
+	}
 }
 
 export default SignupForm;
--- src/components/signup/signup.js
+++ src/components/signup/signup.js
@@ -1,4 +1,4 @@
-import React, { Component }from 'react'
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
@@ -6,38 +6,37 @@ import bg from './video/bg.mp4';
 import SignupForm from '../forms/SignupForm';
 import userSignupRequest from '../../actions/signupActions';
 
-class Signup extends Component{
+class Signup extends Component {
+  render() {
+    const { userSignupRequest } = this.props;
+    return (
+      <div className="ch-login">
+        <div className="intro">
+          <video loop muted autoPlay className="fullscreen-bg__video">
+            <source src={bg} type="video/mp4" />
+          </video>
+        </div>
 
-    render(){
-        const { userSignupRequest } = this.props;
-        return (
-            <div className="ch-login">
-                <div className="intro">
-                    <video loop muted autoPlay className="fullscreen-bg__video">
-                        <source src={bg} type="video/mp4" />
-                    </video>                   
-                </div>
+        <div className="forms">
+          <div className="logo">
+            <h2>chumvi</h2>
+          </div>
 
-                <div className="forms">
-                    <div className="logo">
-                        <h2>chumvi</h2>
-                    </div>
-
-                    <div className="form">
-                        <div className="tab">
-                            <button>Login</button>
-                            <button className="active">SignUp</button>
-                        </div>
-                        <SignupForm  userSignupRequest={userSignupRequest}/>
-                    </div>
-                </div>
+          <div className="form">
+            <div className="tab">
+              <button>Login</button>
+              <button className="active">SignUp</button>
             </div>
-        );
-    }
+            <SignupForm userSignupRequest={userSignupRequest} />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 Signup.propTypes = {
-    userSignupRequest: PropTypes.func.isRequired
-}
+  userSignupRequest: PropTypes.func.isRequired,
+};
 
 export default connect(null, { userSignupRequest })(Signup);
